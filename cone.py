9505549c0d2680918716250b655c7c1fe28a275f
Muhammad Saqib <97337837+MuhammadSaqibms@users.noreply.github.com>
2023-04-08 21:57:30 +0500

    Create cone.py

---
+++ cone.py
@@ -0,0 +1,9 @@
+n =10
+for i in range(n,1,-1):
+    for space in range(0,n-i):
+        print(' ',end='')
+    for j in range(i,2*i-1):
+        print('*', end='')
+    for j in range(1,i-1):
+        print('*',end='')
+    print()
